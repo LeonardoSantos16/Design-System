36ca0e2ea2ad5e7db22f643b9e40d8d527b660ca
leonardo <leodsmf@gmail.com>
2024-09-05 10:25:59 -0300

    documentação das corres commit

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,10 +1,14 @@
 import type { Preview } from "@storybook/react";
-
+import { themes } from '@storybook/theming'
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+  },
     backgrounds: {
       default: 'dark'
     },
+    
     controls: {
       matchers: {
         color: /(background|color)$/i,
